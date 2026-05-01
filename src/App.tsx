6e9f7de987d322afc5f5b83622e8930b3030f2b2
Ali Bakay <ali.bakay@outlook.be>
2026-05-01 15:40:13 +0200

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Test is {count}
         </button>
       </section>
 
